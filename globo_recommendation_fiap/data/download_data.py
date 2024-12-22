e7e20035f149c2ee26f95d575d85af76737aa588
brisa <brisamnascimento@gmail.com>
2024-12-22 16:03:12 -0300

    (data) - Adding logic to combine all raw csv files described in dataset_description, upload to data lake and a inicial function to download data from blob.

---
+++ globo_recommendation_fiap/data/download_data.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from io import StringIO
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from utils.lake_connector import connect_to_adls
+
+def download_from_adls(container_name: str, file_path:str):
+    """
+    Download the intended file from Datalake blob.
+
+    Params:
+    container_name (str): Specific container that usually represents a layer in a Data Lake.
+    file_path: (str): The full path to the intended file within a container.
+    """
+    blob_name = file_path
+    blob = connect_to_adls(container_name, blob_name)
+    blob_data = blob.download_blob()
+    blob_content = blob_data.readall()
+    csv_data = blob_content.decode('utf-8')
+    data = pd.read_csv(StringIO(csv_data))
+    print('Download from lake was sucessful.')
+    return data
+
+if __name__ == "__main__":
+    container_name = "bronze"
+    file_path = "raw/globo/validacao_k.csv"
+    data = download_from_adls(container_name, file_path)
+    print(data.head())
---
+++ globo_recommendation_fiap/utils/lake_connector.py
@@ -0,0 +1,30 @@
+from azure.storage.blob import BlobClient
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from utils.settings import Settings
+
+def connect_to_adls(container_name: str, blob_name: str):
+    """
+    Connect to Azure Data Lake storage.
+
+    Params:
+    container_name (str): Specific container that usually represents a layer in a Data Lake.
+    blob_name (str): The full path to the intended file within a container.
+    """
+    connection_string = Settings().BLOB_API_KEY
+    blob = BlobClient.from_connection_string(
+        conn_str=connection_string,
+        container_name=container_name,
+        blob_name=blob_name
+    )
+    return blob
+
+
+if __name__ == "__main__":
+    container_name = "bronze"
+    blob_name = 'raw/globo/teste.csv'
+    blob = connect_to_adls(container_name, blob_name)
+    print(blob)
