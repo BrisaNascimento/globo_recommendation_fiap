104ae1e6a8c02e2ffccb933c3dd1a3e25188d0f3
Júlio Bernardes <juliocd.bernardes@gmail.com>
2025-01-30 21:21:04 -0300

    Adding colab filter experiments with surprise

--- globo_recommendation_fiap/ml_model_training/als_colaborative_filter.py
+++ globo_recommendation_fiap/ml_model_training/als_colaborative_filter.py
@@ -5,35 +5,35 @@ import numpy as np
 import pandas as pd
 from implicit.als import AlternatingLeastSquares
 from scipy.sparse import coo_matrix
+from sklearn.model_selection import train_test_split
 
-from globo_recommendation_fiap.\
-    ml_model_training.matrix_generator import (
+from globo_recommendation_fiap.ml_model_training.matrix_generator import (
     MatrixGenerator,
 )
 
 
 class AlsCollaborativeFilter:
-
-    def __init__(self,
-                 data: pd.DataFrame,
-                 param_grid: dict,
-                 row: str,
-                 column: str,
-                 value: str
-                 ) -> None:
+    def __init__(
+        self,
+        data: pd.DataFrame,
+        param_grid: dict,
+        row: str,
+        column: str,
+        value: str,
+    ) -> None:
         self.raw_data = data
         self.param_grid = param_grid
         self.set_train_data(row=row, column=column, value=value)
 
-    def set_train_data(self,
-                       row: str,
-                       column: str,
-                       value: str) -> coo_matrix:
-        m_gen = MatrixGenerator(self.raw_data)
-        self.train_data = m_gen.get_matrix(
-            column=column,
-            row=row,
-            value=value
+    def set_train_data(self, row: str, column: str, value: str) -> coo_matrix:
+        train, test = train_test_split(self.raw_data, test_size=0.2)
+        train_m_gen = MatrixGenerator(train)
+        self.train_data = train_m_gen.get_matrix(
+            column=column, row=row, value=value
+        )[2].tocsr()
+        test_m_gen = MatrixGenerator(test)
+        self.test_data = test_m_gen.get_matrix(
+            column=column, row=row, value=value
         )[2].tocsr()
 
     def train(self, k: int = 5) -> None:
@@ -42,36 +42,53 @@ class AlsCollaborativeFilter:
         for params in itertools.product(*self.param_grid.values()):
             factors, regularization, iterations = params
             with mlflow.start_run():
-                mlflow.log_param("factors", factors)
-                mlflow.log_param("regularization", regularization)
-                mlflow.log_param("iterations", iterations)
+                mlflow.log_param('factors', factors)
+                mlflow.log_param('regularization', regularization)
+                mlflow.log_param('iterations', iterations)
                 model = AlternatingLeastSquares(
                     factors=factors,
                     regularization=regularization,
-                    iterations=iterations
+                    iterations=iterations,
                 )
                 model.fit(self.train_data)
                 sparsity = 1.0 - (
-                    self.train_data.count_nonzero() /
-                    (num_users * num_pages))
+                    self.train_data.count_nonzero() / (num_users * num_pages)
+                )
                 precision = self.precision_at_k(model, k)
-                mlflow.log_metric("sparsity", sparsity)
-                mlflow.log_metric(f"precision_at_{k}", precision)
-                mlflow.sklearn.log_model(model, "als_model")
+                mlflow.log_metric('sparsity', sparsity)
+                mlflow.log_metric(f'precision_at_{k}', precision)
+                mlflow.sklearn.log_model(model, 'als_model')
 
     def precision_at_k(self, model: AlternatingLeastSquares, k: int) -> float:
+        """
+        Calculates the precision@k metric using separate train and test data.
+
+        Args:
+            model: Trained AlternatingLeastSquares model.
+            k: Number of top-k recommendations to consider.
+
+        Returns:
+            float: Average precision@k across all users in the test data.
+        """
+
         precisions = []
-        num_users = self.train_data.shape[0]
+        num_users = self.test_data.shape[0]
 
         for user_idx in range(num_users):
-            user_interactions = self.train_data[user_idx].toarray().flatten()
-            relevant_items = set(np.where(user_interactions > 0)[0])
+            # Get known interactions for the user in the test data
+            test_interactions = self.test_data[user_idx].toarray().flatten()
+            relevant_items = set(np.where(test_interactions > 0)[0])
+
             if len(relevant_items) == 0:
                 continue
+
+            # Get recommendations for the user
             recommended, _ = model.recommend(
                 user_idx, self.train_data[user_idx], N=k
-                )
+            )
             recommended_set = set(recommended)
+
+            # Calculate precision
             precision = len(relevant_items & recommended_set) / k
             precisions.append(precision)
 
@@ -86,7 +103,7 @@ if __name__ == '__main__':
     # }
     path = 'globo_recommendation_fiap/data/challenge_files/local'
     data = pd.read_parquet(f'{path}/user_colab_filter.parquet')
-    data = data.head(2000)
+    data = data.head(10000)
     row = 'userId'
     column = 'history'
     value = 'engagement_score_pca'
@@ -97,14 +114,10 @@ if __name__ == '__main__':
     param_grid = {
         'factors': factors,
         'regularization': regularization,
-        'iterations': iterations
+        'iterations': iterations,
     }
     als_train = AlsCollaborativeFilter(
-        data=df,
-        param_grid=param_grid,
-        row=row,
-        column=column,
-        value=value
+        data=df, param_grid=param_grid, row=row, column=column, value=value
     )
 
     als_train.train()
--- globo_recommendation_fiap/ml_model_training/surprise_colaborative_filtering.py
+++ globo_recommendation_fiap/ml_model_training/surprise_colaborative_filtering.py
@@ -4,67 +4,146 @@ import pandas as pd
 from surprise import SVD, Dataset, Reader
 from surprise.model_selection import cross_validate
 
-data = pd.read_parquet('file.parket')
-
-reader = Reader(rating_scale=(1, 0))
-data = Dataset.load_from_df(
-    data[['userId', 'history', 'read_flag']],
-    reader
-)
+# reader = Reader(rating_scale=(1, 0))
+# data = Dataset.load_from_df(data[['userId', 'history', 'read_flag']], reader)
 
 
-def objective(trial):
-    n_factors = trial.suggest_int('n_factors', 50, 200)
-    n_epochs = trial.suggest_int('n_epochs', 10, 50)
-    lr_all = trial.suggest_float('lr_all', 0.001, 0.1)
-    reg_all = trial.suggest_int('reg_all', 0.01, 0.1)
+# def objective(trial):
+#     n_factors = trial.suggest_int('n_factors', 50, 200)
+#     n_epochs = trial.suggest_int('n_epochs', 10, 50)
+#     lr_all = trial.suggest_float('lr_all', 0.001, 0.1)
+#     reg_all = trial.suggest_int('reg_all', 0.01, 0.1)
 
-    model = SVD(
-        n_factors=n_factors,
-        n_epochs=n_epochs,
-        lr_all=lr_all,
-        reg_all=reg_all)
+#     model = SVD(
+#         n_factors=n_factors,
+#         n_epochs=n_epochs,
+#         lr_all=lr_all,
+#         reg_all=reg_all
+#     )
 
-    cv_results = cross_validate(
-        model, data, measures=['RMSE'], cv=3, verbose=False)
+#     cv_results = cross_validate(
+#         model, data, measures=['RMSE'], cv=3, verbose=False
+#     )
 
-    with mlflow.start_run(nested=True):
-        mlflow.log_params({
-            'n_factors': n_factors,
-            'n_epochs': n_epochs,
-            'lr_all': lr_all,
-            'reg_all': reg_all
-        })
-        mlflow.log_metrics(
-            'mean_rmse',
-            cv_results['test_rmse'].mean()
-        )
-        return cv_results['test_rmse'].mean()
-
-
-# Set Experiment
-mlflow.set_experiment('Collaborative_Filtering_Optuna')
-
-# Run
-with mlflow.start_run():
-    study = optuna.create_study(direction='minimize')
-    study.optimize(objective, n_trials=20)
-
-# Log Best parameters and metrics
-best_params = study.best_params
-best_rmse = study.best_value
-
-mlflow.log_params(best_params)
-mlflow.log_metric('best_rmse', best_rmse)
+#     with mlflow.start_run(nested=True):
+#         mlflow.log_params({
+#             'n_factors': n_factors,
+#             'n_epochs': n_epochs,
+#             'lr_all': lr_all,
+#             'reg_all': reg_all,
+#         })
+#         mlflow.log_metrics('mean_rmse', cv_results['test_rmse'].mean())
+#         return cv_results['test_rmse'].mean()
 
 
-# Train Final Model
-best_model = SVD(**best_params)
-trainset = data.build_full_trainset()
-best_model.fit(trainset)
+# # Set Experiment
+# mlflow.set_experiment('Collaborative_Filtering_Optuna')
 
-mlflow.sklearn.log_model(best_model, "colab_filter_model")
+# # Run
+# with mlflow.start_run():
+#     study = optuna.create_study(direction='minimize')
+#     study.optimize(objective, n_trials=20)
+
+# # Log Best parameters and metrics
+# best_params = study.best_params
+# best_rmse = study.best_value
+
+# mlflow.log_params(best_params)
+# mlflow.log_metric('best_rmse', best_rmse)
+
+
+# # Train Final Model
+# best_model = SVD(**best_params)
+# trainset = data.build_full_trainset()
+# best_model.fit(trainset)
+
+# mlflow.sklearn.log_model(best_model, 'colab_filter_model')
 
 
 class ColaborativeFilterExperiment:
-    pass
+    def __init__(
+        self,
+        experiment_name: str,
+        data: pd.DataFrame,
+        direction: str = 'minimize',
+        trials: int = 20,
+    ):
+        self._experiment_name = experiment_name
+        self._direction = direction
+        self._data = data
+        self._trials = trials
+
+    def objective(self, trial):
+        n_factors = trial.suggest_int('n_factors', 50, 200)
+        n_epochs = trial.suggest_int('n_epochs', 10, 50)
+        lr_all = trial.suggest_float('lr_all', 0.001, 0.1)
+        reg_all = trial.suggest_int('reg_all', 0.01, 0.1)
+
+        model = SVD(
+            n_factors=n_factors,
+            n_epochs=n_epochs,
+            lr_all=lr_all,
+            reg_all=reg_all,
+        )
+
+        cv_results = cross_validate(
+            model, self._data, measures=['RMSE'], cv=3, verbose=False
+        )
+
+        with mlflow.start_run(nested=True):
+            mlflow.log_params({
+                'n_factors': n_factors,
+                'n_epochs': n_epochs,
+                'lr_all': lr_all,
+                'reg_all': reg_all,
+            })
+            mlflow.log_metrics({'mean_rmse': cv_results['test_rmse'].mean()})
+            return cv_results['test_rmse'].mean()
+
+    def train_best(self):
+        # Train Final Model
+        best_model = SVD(**self._best_params)
+        trainset = self._data.build_full_trainset()
+        best_model.fit(trainset)
+        mlflow.sklearn.log_model(best_model, 'colab_filter_model')
+
+    def run_experiment(
+        self,
+        rating_field: str = 'flag_read',
+        rating_a: int = 0,
+        rating_b: int = 1,
+    ):
+        reader = Reader(rating_scale=(rating_a, rating_b))
+        self._data = Dataset.load_from_df(
+            self._data[['userId', 'history', rating_field]], reader
+        )
+        # Set Experiment
+        mlflow.set_experiment(self._experiment_name)
+
+        # Run
+        with mlflow.start_run():
+            study = optuna.create_study(direction=self._direction)
+            study.optimize(self.objective, n_trials=self._trials)
+
+        # Log Best parameters and metrics
+        self._best_params = study.best_params
+        self._best_rmse = study.best_value
+
+        mlflow.log_params(self._best_params)
+        mlflow.log_metric('best_rmse', self._best_rmse)
+
+        self.train_best()
+
+
+if __name__ == '__main__':
+    path = 'globo_recommendation_fiap/data/challenge_files/local'
+    data = pd.read_parquet(f'{path}/acessos_filtrados.parquet')
+    train_data = data[['userId','history','flag_read']]
+    train_data_test = train_data.iloc[:10000]
+    experiment = "Teste_classe_surprise"
+    ml_experiment = ColaborativeFilterExperiment(
+        experiment_name=experiment,
+        data=train_data)
+    
+    ml_experiment.run_experiment()
+
