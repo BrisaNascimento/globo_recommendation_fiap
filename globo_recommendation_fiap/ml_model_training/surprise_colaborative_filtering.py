99c558b7e4079e2b130da849c4661dcb5490b526
FlavioPasquetti <113318241+FlavioPasquetti@users.noreply.github.com>
2025-02-16 15:26:23 -0300

    Surprise colaborative filtering optimization

--- globo_recommendation_fiap/ml_model_training/surprise_colaborative_filtering.py
+++ globo_recommendation_fiap/ml_model_training/surprise_colaborative_filtering.py
@@ -4,61 +4,6 @@ import pandas as pd
 from surprise import SVD, Dataset, Reader
 from surprise.model_selection import cross_validate
 
-# reader = Reader(rating_scale=(1, 0))
-# data = Dataset.load_from_df(data[['userId', 'history', 'read_flag']], reader)
-
-
-# def objective(trial):
-#     n_factors = trial.suggest_int('n_factors', 50, 200)
-#     n_epochs = trial.suggest_int('n_epochs', 10, 50)
-#     lr_all = trial.suggest_float('lr_all', 0.001, 0.1)
-#     reg_all = trial.suggest_int('reg_all', 0.01, 0.1)
-
-#     model = SVD(
-#         n_factors=n_factors,
-#         n_epochs=n_epochs,
-#         lr_all=lr_all,
-#         reg_all=reg_all
-#     )
-
-#     cv_results = cross_validate(
-#         model, data, measures=['RMSE'], cv=3, verbose=False
-#     )
-
-#     with mlflow.start_run(nested=True):
-#         mlflow.log_params({
-#             'n_factors': n_factors,
-#             'n_epochs': n_epochs,
-#             'lr_all': lr_all,
-#             'reg_all': reg_all,
-#         })
-#         mlflow.log_metrics('mean_rmse', cv_results['test_rmse'].mean())
-#         return cv_results['test_rmse'].mean()
-
-
-# # Set Experiment
-# mlflow.set_experiment('Collaborative_Filtering_Optuna')
-
-# # Run
-# with mlflow.start_run():
-#     study = optuna.create_study(direction='minimize')
-#     study.optimize(objective, n_trials=20)
-
-# # Log Best parameters and metrics
-# best_params = study.best_params
-# best_rmse = study.best_value
-
-# mlflow.log_params(best_params)
-# mlflow.log_metric('best_rmse', best_rmse)
-
-
-# # Train Final Model
-# best_model = SVD(**best_params)
-# trainset = data.build_full_trainset()
-# best_model.fit(trainset)
-
-# mlflow.sklearn.log_model(best_model, 'colab_filter_model')
-
 
 class ColaborativeFilterExperiment:
     def __init__(
@@ -74,21 +19,36 @@ class ColaborativeFilterExperiment:
         self._trials = trials
 
     def objective(self, trial):
-        n_factors = trial.suggest_int('n_factors', 50, 200)
-        n_epochs = trial.suggest_int('n_epochs', 10, 50)
-        lr_all = trial.suggest_float('lr_all', 0.001, 0.1)
-        reg_all = trial.suggest_int('reg_all', 0.01, 0.1)
+        n_factors = trial.suggest_int('n_factors', 10, 300)
+        n_epochs = trial.suggest_int('n_epochs', 10, 300)
+        lr_all = trial.suggest_float('lr_all', 0.001, 0.2)
+        # estava como suggest_int
+        reg_all = trial.suggest_float('reg_all', 0.01, 0.1)
+        # init_mean = trial.suggest_float('init_mean', 0, 0.1)
+        # init_std_dev = trial.suggest_float('init_std_dev', 0.01, 0.1)
 
         model = SVD(
             n_factors=n_factors,
             n_epochs=n_epochs,
             lr_all=lr_all,
             reg_all=reg_all,
+            # init_mean=init_mean,
+            # init_std_dev=init_std_dev,
         )
 
         cv_results = cross_validate(
-            model, self._data, measures=['RMSE'], cv=3, verbose=False
+            model, self._data, measures=['RMSE', 'MAE'], cv=3, verbose=False
         )
+
+        # Print outras métricas
+        # Média do erro absoluto
+        print(f'MAE: {cv_results["test_mae"].mean()}')
+        # Média do erro quadrático médio
+        print(f'RMSE: {cv_results["test_rmse"].mean()}')
+        # Média da métrica R2
+        # print(f"R2: {cv_results['test_r2'].mean()}")
+        # Média do erro percentual absoluto médio
+        # print(f"MAPE: {cv_results['test_mape'].mean()}")
 
         with mlflow.start_run(nested=True):
             mlflow.log_params({
@@ -96,6 +56,8 @@ class ColaborativeFilterExperiment:
                 'n_epochs': n_epochs,
                 'lr_all': lr_all,
                 'reg_all': reg_all,
+                # 'init_mean': init_mean,
+                # 'init_std_dev': init_std_dev,
             })
             mlflow.log_metrics({'mean_rmse': cv_results['test_rmse'].mean()})
             return cv_results['test_rmse'].mean()
@@ -114,8 +76,10 @@ class ColaborativeFilterExperiment:
         rating_b: int = 0,
     ):
         reader = Reader(rating_scale=(rating_a, rating_b))
+        # self._data = Dataset.load_from_df(self._data[['userId', 'history',
+        # rating_field]],reader)
         self._data = Dataset.load_from_df(
-            self._data[['userId', 'history', rating_field]], reader
+            self._data[['User', 'Page', rating_field]], reader
         )
         # Set Experiment
         mlflow.set_experiment(self._experiment_name)
@@ -127,6 +91,7 @@ class ColaborativeFilterExperiment:
 
         # Log Best parameters and metrics
         self._best_params = study.best_params
+        print('parametros: ', self._best_params)
         self._best_rmse = study.best_value
 
         mlflow.log_params(self._best_params)
@@ -145,4 +110,8 @@ if __name__ == '__main__':
         experiment_name=experiment, data=train_data_test
     )
 
-    ml_experiment.run_experiment()
+    ml_experiment.run_experiment(
+        rating_field='Score',
+        rating_a=train_data_test['Score'].max(),
+        rating_b=train_data_test['Score'].min(),
+    )
