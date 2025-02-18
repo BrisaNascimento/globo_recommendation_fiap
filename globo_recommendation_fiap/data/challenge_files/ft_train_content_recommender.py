4e391948426704f79691f7a770aa352bf762fdab
brisa <brisamnascimento@gmail.com>
2025-02-17 22:23:58 -0300

    lint format

--- globo_recommendation_fiap/data/challenge_files/ft_train_content_recommender.py
+++ globo_recommendation_fiap/data/challenge_files/ft_train_content_recommender.py
@@ -1,23 +1,30 @@
-import pandas as pd
 import os
 import sys
+
+import pandas as pd
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from globo_recommendation_fiap.data.download_data import download_from_adls
-
 from globo_recommendation_fiap.data.upload_data import upload_to_dl
 from globo_recommendation_fiap.utils.data_utils import (
-                                                        clean_column_spaces, 
-                                                        convert_to_date
-                                                        )
+                                                        clean_column_spaces,
+                                                        convert_to_date,
+)
 
 # CREATE LAST ENTRIES VIEW
 
 # Load Data from Lake
-df_acessos = download_from_adls('silver', 'globo/acessos_flag_read/acessos.parquet')
-df_itens = download_from_adls('bronze', 'raw/globo/itens/itens.parquet')[['page', 'issued']]
-df_noticias = download_from_adls('silver', 'globo/content_embbeding/content_embedding_cleaned.parquet')
+df_acessos = download_from_adls(
+    'silver',
+    'globo/acessos_flag_read/acessos.parquet'
+    )
+df_itens = download_from_adls(
+    'bronze',
+    'raw/globo/itens/itens.parquet')[['page', 'issued']]
+df_noticias = download_from_adls(
+    'silver',
+    'globo/content_embbeding/content_embedding_cleaned.parquet')
 
 # Clean col spaces
 df_itens = clean_column_spaces(df_itens, ['page'])
@@ -31,19 +38,45 @@ df_itens.rename(columns={'page': 'page_2'}, inplace=True)
 df_acessos.rename(columns={'timestamp_date': 'date'}, inplace=True)
 
 # Join embbeding information and issued date
-df_noticias = df_noticias.merge(df_itens, left_on='page', right_on='page_2', how='inner')
-df_noticias = df_noticias[['page', 'content_embbeding', 'issued', 'issued_date']]
+df_noticias = df_noticias.merge(
+    df_itens,
+    left_on='page', right_on='page_2',
+    how='inner'
+    )
+df_noticias = df_noticias[
+    ['page', 'content_embbeding', 'issued', 'issued_date']
+    ]
 
 # Join access information to issued date
-acessos = df_acessos.merge(df_noticias, left_on='history', right_on='page', how='inner')
-acessos = acessos[['userId', 'history', 'content_embbeding','timestamp', 'date', 'issued', 'issued_date']]
+acessos = df_acessos.merge(
+    df_noticias,
+    left_on='history', right_on='page',
+    how='inner')
+acessos = acessos[
+        [
+        'userId',
+        'history',
+        'content_embbeding',
+        'timestamp',
+        'date',
+        'issued',
+        'issued_date'
+        ]
+        ]
 
-# Clean access and keeps only data accessed before news creation date - Consistency
+# Clean access and keeps only data accessed before news creation date
+# For Consistency
 acessos_cleanned = acessos[acessos['issued'] <= acessos['timestamp']].copy()
 
 # Keep last 5 entries for user
-acessos_cleanned.loc[:, 'rank'] = acessos_cleanned.groupby('userId')['timestamp'].rank(method='first', ascending=False)
-ult_acessos = acessos_cleanned[acessos_cleanned['rank'] <= 5][['userId', 'history', 'content_embbeding', 'timestamp', 'date']]
+
+min_last_entries = 5
+acessos_cleanned.loc[:, 'rank'] = acessos_cleanned.groupby(
+    'userId'
+    )['timestamp'].rank(method='first', ascending=False)
+ult_acessos = acessos_cleanned[acessos_cleanned['rank'] <= min_last_entries][
+    ['userId', 'history', 'content_embbeding', 'timestamp', 'date']
+    ]
 ult_acessos.rename(columns={'date': 'access_date'}, inplace=True)
 ult_acessos.reset_index(drop=True, inplace=True)
 
@@ -52,23 +85,46 @@ upload_to_dl(ult_acessos, 'silver', 'globo/acessos/ultimos_acessos.parquet')
 # CREATE LAST NEWS VIEW
 
 # More recent news (created after 13-08-2022)
-noticias = df_noticias[df_noticias['issued_date'] >= pd.to_datetime('2022-08-13').date()]
-noticias.reset_index(drop=True,inplace=True)
+noticias = df_noticias[
+    df_noticias['issued_date'] >= pd.to_datetime('2022-08-13').date()
+    ]
+noticias.reset_index(drop=True, inplace=True)
 
 upload_to_dl(noticias, 'silver', 'globo/noticias/ultimas_noticias.parquet')
 
 # CRATE TOP 20 MORE ENTRIED NEWS
 
 # Create ranking for 20 more entried news
-mais_acessadas = acessos_cleanned.groupby('history').size().reset_index(name='access_count')
+mais_acessadas = acessos_cleanned.groupby(
+    'history'
+    ).size().reset_index(name='access_count')
 mais_acessadas = mais_acessadas.sort_values(by='access_count', ascending=False)
-mais_acessadas['access_rank'] = mais_acessadas['access_count'].rank(method='first', ascending=False)
-mais_acessadas = mais_acessadas.merge(noticias, left_on='history', right_on='page', how='inner')
-mais_acessadas = mais_acessadas[mais_acessadas['issued_date'] >= pd.to_datetime('2022-08-13').date()]
-mais_acessadas['last_access_rank'] = mais_acessadas['access_count'].rank(method='first', ascending=False)
-noticias_mais_acessadas = mais_acessadas[mais_acessadas['last_access_rank'] <= 20]
+mais_acessadas['access_rank'] = mais_acessadas['access_count'].rank(
+    method='first', ascending=False
+    )
+mais_acessadas = mais_acessadas.merge(
+    noticias,
+    left_on='history', right_on='page',
+    how='inner')
+mais_acessadas = mais_acessadas[
+    mais_acessadas['issued_date'] >= pd.to_datetime('2022-08-13').date()
+    ]
+mais_acessadas['last_access_rank'] = mais_acessadas['access_count'].rank(
+    method='first', ascending=False
+    )
+rank_top = 20
+noticias_mais_acessadas = mais_acessadas[
+    mais_acessadas['last_access_rank'] <= rank_top
+    ]
 
-noticias_mais_acessadas = noticias_mais_acessadas[['history', 'last_access_rank']]
-noticias_mais_acessadas['last_access_rank'] = noticias_mais_acessadas['last_access_rank'].astype(int)
+noticias_mais_acessadas = noticias_mais_acessadas[
+    ['history', 'last_access_rank']
+]
+noticias_mais_acessadas['last_access_rank'] = noticias_mais_acessadas[
+    'last_access_rank'
+    ].astype(int)
 
-upload_to_dl(noticias_mais_acessadas, 'silver', 'globo/noticias_mais_acessadas/mais_acessadas.parquet')
+upload_to_dl(
+    noticias_mais_acessadas,
+    'silver', 'globo/noticias_mais_acessadas/mais_acessadas.parquet'
+    )
--- globo_recommendation_fiap/utils/data_utils.py
+++ globo_recommendation_fiap/utils/data_utils.py
@@ -1,29 +1,38 @@
-import pandas as pd
 import re
+
+import pandas as pd
+
 
 # Function to load local data in parquet format
 def load_parquet(path):
-     return pd.read_parquet(path)
+    return pd.read_parquet(path)
+
 
 # Function to load local data in csv format
 def load_csv(file_path):
     return pd.read_csv(file_path)
 
+
 # Function to remove unwanted columns
 def drop_cols(df, columns_to_drop):
     return df.drop(columns=columns_to_drop, errors='ignore')
 
+
 # Function to clean spaces and other regex in specific columns
 def clean_text_columns(df, columns, pattern="\\s+", replacement=""):
     for col in columns:
-        df[col] = df[col].astype(str).apply(lambda x: re.sub(pattern, replacement, x))
+        df[col] = df[col].astype(str).apply(
+            lambda x: re.sub(pattern, replacement, x)
+            )
     return df
+
 
 # Function to clean spaces in specific columns
 def clean_column_spaces(df, columns):
     for col in columns:
         df[col] = df[col].str.replace(r"\s+", "", regex=True)
     return df
+
 
 # Function to convert columns in datetime to date
 def convert_to_date(df, columns):
